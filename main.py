121ecefaf554290c3ee9e4ee31d3c641e3ef5e8f
Anishka Chauhan <email.friendme@gmail.com>
2025-11-25 09:55:56 -0800

    finished timer

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask
-from flask import render_template
- 
+from flask import Flask, Response, render_template, request, stream_with_context
+import time
+
 app = Flask(__name__) 
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/timer')
+def timer_page():
+    return render_template('timer.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
